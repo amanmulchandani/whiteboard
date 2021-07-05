5470a92c89a9141249f8034e89dc39504490039c
Aman Mulchandani <mulchandaniaman@gmail.com>
2021-07-05 17:53:42 +0530

    Final Done

--- assets/js/index.js
+++ assets/js/index.js
@@ -121,3 +121,18 @@ clear.addEventListener('click', (e) => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     makeBgWhite();
 });
+
+
+
+//Instractions
+const open = document.getElementById('open');
+const modal_container = document.getElementById('modal_container')
+const close = document.getElementById('close');
+
+open.addEventListener('click', () => {
+    modal_container.classList.add('show');
+});
+
+close.addEventListener('click', () => {
+    modal_container.classList.remove('show');
+});
